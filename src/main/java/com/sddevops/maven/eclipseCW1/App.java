c6067a48f2eda894f9c8deb7b5ca2461279d893b
nqabr <justnoonesodontbother@gmail.com>
2023-07-13 14:27:55 -0700

    Initial Commit

---
+++ src/main/java/com/sddevops/maven/eclipseCW1/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.maven.eclipseCW1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
